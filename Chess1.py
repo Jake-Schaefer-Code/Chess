a1c96afd7117f2ea787507cd64ec9c7ec5ec6245
Jake <87781057+Snakej07@users.noreply.github.com>
2023-02-03 15:09:32 -0600

    Add files via upload

---
+++ Chess1.py
@@ -0,0 +1,15 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL.Image import Image
+from PIL import Image
+
+Im = Image.open("chessboard.png")
+Im.show()
+
+class Board:
+    def __init__(self, screen, x, y, coin_image):
+        self.screen = screen
+        self.x = x
+        self.y = y
+    ImBoard = Image.open("chessboard.png")
+    ImBoard.show()
