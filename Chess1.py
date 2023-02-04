826a2dea67975c289dd238372b8197a196f5d2db
Jake <87781057+Snakej07@users.noreply.github.com>
2023-02-03 18:22:05 -0600

    Update Chess1.py

--- Chess1.py
+++ Chess1.py
@@ -2,16 +2,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL.Image import Image
 from PIL import Image
+from tkinter import *
+from tkinter import ttk
+
+root = Tk()
 
 Im = Image.open("chessboard.png")
 Im.show()
 
 class Board:
-    def __init__(self, screen, x, y, coin_image):
+    def __init__(self, screen, x, y):
         self.screen = screen
         self.x = x
         self.y = y
-    ImBoard = Image.open("chessboard.png")
-    ImBoard.show()
+        self.tiles = {} #dictionary of the tiles
+        self.white_pieces = {} #dictionary of the white pieces
+        self.black_pieces = {} #dictionary of the black pieces
+    
+    
+    def select_piece(self):
+        
+    def setup(self): #sets up the pieces in the startin positions
+        
+    
+    
 class Game:
     
