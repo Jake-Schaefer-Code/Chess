d269e6d765b5c34b4b91d015cd902b5ac8a16e50
Jake <87781057+Snakej07@users.noreply.github.com>
2023-02-15 14:46:06 -0600

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,6 +1,13 @@
 import pygame
 import sys
 
+#universal variables? can we put this somewhere else like inside main or will it not work?
+coordslist = []
+coordsx = {}
+coordsy = {}
+squarecolor = {}
+
+
 class Board:
     def __init__(self, screen, x, y, image):
         self.screen = screen
@@ -16,21 +23,44 @@ class Board:
     def coordinates(self):
         self.ycoords = ['a','b','c','d','e','f','g','h']
         for i in range(8):
+            coordsx[((i)*(self.image.get_width()/8),(i+1)*(self.image.get_width()/8))] = i + 1
             for j in range(8):
                 coordslist.append(self.ycoords[j]+str(i+1))
-                
-                #coordsx[(i+1)] = ((i)*(self.image.get_width()/8),(i+1)*(self.image.get_width()/8)) #defines each square to be 1/8 of the board
-                #coordsy[self.ycoords[j]] = ((j)*(self.image.get_height()/8),(j+1)*(self.image.get_height()/8))
-                #if (i+1)%2 == 1:
-                    #self.square_color = "black"
-                #elif (i+1)%2 == 0:
-                    #self.square_color = "white"
+                coordsy[((j)*(self.image.get_width()/8),(j+1)*(self.image.get_width()/8))] = self.ycoords[(-(j+1))]                
+                if (i+1)%2 == 1 and (8-j)%2 == 1:
+                    squarecolor[self.ycoords[-(j+1)] + str(i+1)] = "black"
+                elif (i+1)%2 == 0 and (8-j)%2 == 0:
+                    squarecolor[self.ycoords[-(j+1)] + str(i+1)] = "black"
+                else:
+                    squarecolor[self.ycoords[-(j+1)] + str(i+1)] = "white"
         print(coordslist)
+        print(coordsx)
+        print(coordsy)
+        
+
+    def get_coords(self, pos):
+        print(pos)
+        x_square = None
+        y_square = None
+        for (k1,k2) in coordsx:
+            if (k1 < pos[0] and k2 > pos[0]):
+                x_square = coordsx[(k1,k2)]
+        for (k1,k2) in coordsy:
+            if (k1 < pos[1] and k2 > pos[1]):
+                y_square = coordsy[(k1,k2)]
+
+        print(str(y_square) + str(x_square))
+        print(squarecolor[str(y_square) + str(x_square)])
 
 
-coordslist = []
-coordsx = {}
-coordsy = {}
+
+class square:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    
+
+
 
 def main():
     pygame.init()
@@ -39,7 +69,7 @@ def main():
     boardimage = pygame.image.load("chessboard.png")
     screen = pygame.display.set_mode((boardimage.get_width(), boardimage.get_height()))
     board1 = Board(screen, 0, 0, boardimage)
-    coords = board1.coordinates()
+    board1.coordinates() #defines the coordiate system
 
     while True:
         board1.draw()
@@ -47,7 +77,13 @@ def main():
             if event.type == pygame.QUIT:
                 sys.exit()
         pos = pygame.mouse.get_pos()
+        if pygame.mouse.get_pressed()[0] == True:
+            pygame.time.wait(100)
+            board1.get_coords(pos)
+        
+        
+        
         pygame.display.update()
-
+        
 
 main()
