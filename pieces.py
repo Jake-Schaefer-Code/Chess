1099407e879d4c3727b57ee0892415a28d36eb3a
Jake <87781057+Snakej07@users.noreply.github.com>
2023-02-15 18:46:39 -0600

    Add files via upload

---
+++ pieces.py
@@ -0,0 +1,6 @@
+import pygame
+import sys
+
+whitepiecelist = ["whiteking.PNG", "whitequeen.PNG", "whiterook.PNG", "whitebishop.PNG", "whiteknight.PNG", "whitepawn.PNG"]
+blackpiecelist = ["blackking.PNG", "blackqueen.PNG", "blackrook.PNG", "blackbishop.PNG", "blackknight.PNG", "blackpawn.PNG"]
+piecelist1 = []
