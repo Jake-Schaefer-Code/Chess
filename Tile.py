c44c15054d4f23fbfcd3d6f79d60bff855e6a5b7
Jake <87781057+Snakej07@users.noreply.github.com>
2023-03-01 00:40:14 -0600

    Add files via upload

---
+++ Tile.py
@@ -0,0 +1,27 @@
+import pygame
+import sys
+
+class Tile:
+    def __init__(self, file, rank, piece = None):
+        self.rank = rank
+        self.file = file
+        self.piece = piece
+    
+    def samecolor(self,color):
+        if self.piece != None:
+            return self.piece.color == color
+        elif self.piece == None:
+            return False
+
+    def emptytile(self):
+        return self.piece == None
+
+    def has_ally(self, color):
+        return self.piece != None and self.piece.color == color
+    
+    
+
+
+
+def inrange(rank, file):
+    return (file >= 0 and file <= 7) and (rank >= 0 and rank <= 7)
