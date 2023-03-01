0d756a7d08eed97bbfeefed9b843734429bb57f9
Jake <87781057+Snakej07@users.noreply.github.com>
2023-03-01 00:39:28 -0600

    Delete pieces.py

--- pieces.py
+++
@@ -1,6 +0,0 @@
-import pygame
-import sys
-
-whitepiecelist = ["whiteking.PNG", "whitequeen.PNG", "whiterook.PNG", "whitebishop.PNG", "whiteknight.PNG", "whitepawn.PNG"]
-blackpiecelist = ["blackking.PNG", "blackqueen.PNG", "blackrook.PNG", "blackbishop.PNG", "blackknight.PNG", "blackpawn.PNG"]
-piecelist1 = []
