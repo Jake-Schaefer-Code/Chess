2f267f9f379f131e649ba6976610bd87ffd12624
Rahim <122849167+Rahim-Hamid@users.noreply.github.com>
2023-03-06 12:17:13 -0600

    Create chessai.py

---
+++ chessai.py
@@ -0,0 +1,24 @@
+checkmate = 1000
+stalemate = 0
+
+valuedict = {"p": 1, "n": 3, "b": 3.5, "r": 5, "q": 10, "k": 0}
+
+def greed_ai():
+    pass
+
+'''def kings_pawn():
+    if white's pawn occupies the square e4 or if the first move is returned as a move to e4:
+      move black pawn to e5
+      
+
+#def caro_kann():
+    if white's pawn occupies the square e4 or if the first move is returned as a move to e4
+     move black pawn to c6
+     then if white moves to square d4 or if white moves Knight to F3:
+        play black pawn to d5 
+        then if white captures black pawn on d5
+
+        elif white 
+
+    if white's move != to anything:
+    call ai function to continue playing or break and call the function
