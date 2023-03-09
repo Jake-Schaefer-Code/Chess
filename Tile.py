c315882c7811c7684478fd65cac41e324bba07fa
Jake <87781057+Snakej07@users.noreply.github.com>
2023-03-08 19:27:03 -0600

    Update Tile.py

--- Tile.py
+++ Tile.py
@@ -1,6 +1,8 @@
 import pygame
 import sys
 
+
+
 class Tile:
     def __init__(self, file, rank, piece = None):
         self.rank = rank
@@ -11,16 +13,22 @@ class Tile:
         if self.piece != None:
             return self.piece.color == color
         elif self.piece == None:
-            return False
+            return False        
 
     def emptytile(self):
         return self.piece == None
-
-    def has_ally(self, color):
-        return self.piece != None and self.piece.color == color
     
-    def has_enemy(self, color):
-        return self.has_piece() and self.piece.color != color
+    def has_piece(self):
+        return self.piece != None
+
+    def has_ally(self, color1):
+        return self.piece != None and self.piece.color == color1
+    
+    def has_enemy(self, color1):
+        return self.piece != None and self.piece.color != color1
+    
+
 
 def inrange(rank, file):
     return (file >= 0 and file <= 7) and (rank >= 0 and rank <= 7)
+
