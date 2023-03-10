5cd1349f22ce5689780f81a7df32700ed961e448
Rahim <122849167+Rahim-Hamid@users.noreply.github.com>
2023-03-10 17:48:46 -0600

    Update pieces.py

--- pieces.py
+++ pieces.py
@@ -1,47 +1,47 @@
 import pygame
 import sys
-piecevals = {"p": 10, "n": 30, "b": 35, "r": 50, "q": 90, "k": 900}
+piecevals = {"p": 10, "n": 30, "b": 35, "r": 50, "q": 90, "k": 900} # initialization for the relative values of each piece in order of priority
 
 
 
-class Pawn:
+class Pawn: # intialization of the pawn class
     def __init__(self, color):
-        self.color = color
-        self.value = piecevals["p"]
-        self.imagename = self.color + "p"
+        self.color = color # allows a color to be assigned to the pawn
+        self.value = piecevals["p"] #accesses the value dictionary written above
+        self.imagename = self.color + "p" #accessing the name of the pawn by combining the color with the letter p 
+        self.dir = -1 if self.color == "w" else 1 #assigns a direction to the piece
+
+class Knight: # intialization of the knight class
+    def __init__(self, color):
+        self.color = color # allows a color to be assigned
+        self.value = piecevals["n"] #accesses the value dictionary written above
+        self.imagename = self.color + "n" #accessing the name of the knight by combining the color with the letter n
         self.dir = -1 if self.color == "w" else 1
 
-class Knight:
+class Bishop: # intialization of the bishop class
     def __init__(self, color):
-        self.color = color
-        self.value = piecevals["n"]
-        self.imagename = self.color + "n"
+        self.color = color # allows a color to be assigned
+        self.value = piecevals["b"] #accesses the value dictionary written above
+        self.imagename = self.color + "b" #accessing the name of the bishop by combining the color with the letter b
         self.dir = -1 if self.color == "w" else 1
 
-class Bishop:
+class Rook: # intialization of the rook class
     def __init__(self, color):
-        self.color = color
-        self.value = piecevals["b"]
-        self.imagename = self.color + "b"
+        self.color = color # allows a color to be assigned
+        self.value = piecevals["r"] #accesses the value dictionary written above
+        self.imagename = self.color + "r" #accessing the name of the rook by combining the color with the letter r
         self.dir = -1 if self.color == "w" else 1
 
-class Rook:
+class Queen: # intialization of the queen class
     def __init__(self, color):
-        self.color = color
-        self.value = piecevals["r"]
-        self.imagename = self.color + "r"
+        self.color = color # allows a color to be assigned
+        self.value = piecevals["q"] #accesses the value dictionary written above
+        self.imagename = self.color + "q" #accessing the name of the queen by combining the color with the letter q
         self.dir = -1 if self.color == "w" else 1
 
-class Queen:
+class King: # intialization of the king class
     def __init__(self, color):
-        self.color = color
-        self.value = piecevals["q"]
-        self.imagename = self.color + "q"
-        self.dir = -1 if self.color == "w" else 1
-
-class King:
-    def __init__(self, color):
-        self.color = color
-        self.value = piecevals["k"]
-        self.imagename = self.color + "k"
+        self.color = color # allows a color to be assigned
+        self.value = piecevals["k"] #accesses the value dictionary written above
+        self.imagename = self.color + "k" #accessing the name of the king by combining the color with the letter k
         self.dir = -1 if self.color == "w" else 1
