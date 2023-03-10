146220c78e013c22a9c83a5b9dcd6e4cc2ae90b8
Rahim <122849167+Rahim-Hamid@users.noreply.github.com>
2023-03-10 17:58:07 -0600

    Update Tile.py

--- Tile.py
+++ Tile.py
@@ -3,32 +3,32 @@ import sys
 
 
 
-class Tile:
+class Tile: #intialization of the Tile class
     def __init__(self, file, rank, piece = None):
-        self.rank = rank
-        self.file = file
-        self.piece = piece
+        self.rank = rank #assigning rows
+        self.file = file #assigning columns
+        self.piece = piece #assigning piece or no piece to each square
     
-    def samecolor(self,color):
+    def samecolor(self,color): #checks to see which side a certain piece belongs to
         if self.piece != None:
             return self.piece.color == color
         elif self.piece == None:
             return False        
 
-    def emptytile(self):
-        return self.piece == None
+    def emptytile(self): #checks to see if a tile is empty
+        return self.piece == None 
     
-    def has_piece(self):
+    def has_piece(self): #checks to see if a tile has a piece
         return self.piece != None
 
-    def has_ally(self, color1):
+    def has_ally(self, color1): #checks to see if a tile has an allied piece for either side
         return self.piece != None and self.piece.color == color1
     
-    def has_enemy(self, color1):
+    def has_enemy(self, color1): #checks to see if a tile has an enemy piece for either side
         return self.piece != None and self.piece.color != color1
     
 
 
-def inrange(rank, file):
+def inrange(rank, file): #defines the boundaries of the 8x8 board with 64 individual squares
     return (file >= 0 and file <= 7) and (rank >= 0 and rank <= 7)
 
